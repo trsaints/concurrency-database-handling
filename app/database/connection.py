01ba20650c4056f3ec9ddd4e0936c49368d1ed71
trsaints <thiago.dev2k3@gmail.com>
2025-10-04 23:13:18 -0300

    update(infra): ensure source generator is properly typed

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,18 +1,18 @@
-import psycopg2
-from psycopg2 import pool
 from contextlib import contextmanager
 from typing import Generator
 from app.config.settings import settings
+from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extensions import connection as pg_connection, cursor as pg_cursor
 
 
 class DatabaseConnection:
-    _connection_pool = None
-    
+    _connection_pool: ThreadedConnectionPool
+
     @classmethod
     def initialize_pool(cls, minconn: int = 1, maxconn: int = 10):
         """Initialize the connection pool."""
         if cls._connection_pool is None:
-            cls._connection_pool = psycopg2.pool.ThreadedConnectionPool(
+            cls._connection_pool = ThreadedConnectionPool(
                 minconn,
                 maxconn,
                 host=settings.database_host,
@@ -21,40 +21,45 @@ class DatabaseConnection:
                 user=settings.database_user,
                 password=settings.database_password
             )
-    
+
     @classmethod
     def close_pool(cls):
         """Close all connections in the pool."""
-        if cls._connection_pool is not None:
-            cls._connection_pool.closeall()
-            cls._connection_pool = None
-    
+        cls._connection_pool.closeall()
+
     @classmethod
     @contextmanager
-    def get_connection(cls) -> Generator:
+    def get_connection(cls) -> Generator[pg_connection]:
         """Get a connection from the pool."""
         if cls._connection_pool is None:
             cls.initialize_pool()
-        
+
         connection = cls._connection_pool.getconn()
+
         try:
             yield connection
+
         finally:
             cls._connection_pool.putconn(connection)
-    
+
     @classmethod
     @contextmanager
-    def get_cursor(cls, commit: bool = False) -> Generator:
+    def get_cursor(cls, commit: bool = False) -> Generator[pg_cursor]:
         """Get a cursor from a pooled connection."""
         with cls.get_connection() as connection:
             cursor = connection.cursor()
+
             try:
                 yield cursor
+
                 if commit:
                     connection.commit()
+
             except Exception as e:
                 connection.rollback()
+
                 raise e
+
             finally:
                 cursor.close()
 
